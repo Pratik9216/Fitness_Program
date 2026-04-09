763a07c4239d7ece55053dfcbc475507a2f7e187
Pratik Nichit <you@example.com>
2026-04-08 22:14:01 -0400

    Eureka Service Initial commit

--- activityservice/activityservice/src/main/java/com/fitness/activityservice/config/MongoConfig.java
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/config/MongoConfig.java
@@ -1,35 +1,29 @@
-//package com.fitness.activityservice.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.mongodb.config.EnableMongoAuditing;
-//
-//@Configuration
-//@EnableMongoAuditing
-//public class MongoConfig {
-//}
-
 package com.fitness.activityservice.config;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.MongoTransactionManager;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableMongoAuditing
-public class MongoConfig extends AbstractMongoClientConfiguration {
+public class MongoConfig {
 
-    @Override
-    protected String getDatabaseName() {
-        return "aiactivityfitness";
-    }
-
-    @Override
-    public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://localhost:27017");
+    @Bean
+    public MongoClientSettings mongoClientSettings() {
+        return MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString("mongodb://localhost:27017/aiactivityfitness"))
+                .applyToSocketSettings(builder ->
+                        builder.readTimeout(0, TimeUnit.MILLISECONDS)
+                                .connectTimeout(10000, TimeUnit.MILLISECONDS)
+                )
+                .applyToServerSettings(builder ->
+                        builder.heartbeatFrequency(60000, TimeUnit.MILLISECONDS)
+                                .minHeartbeatFrequency(500, TimeUnit.MILLISECONDS)
+                )
+                .build();
     }
 }
